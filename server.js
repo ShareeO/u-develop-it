1937b07990b98805d05521615077c085870a2d5d
ShareeO <selena74354@yahoo.com>
2021-05-09 21:01:26 -0500

    server.js file

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
 });
 
-app.get('/', (req, res) => {
-    res.json({
-message: 'Hello World'
-    });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
